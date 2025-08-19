13dd54b5240a399f34df03bffd3a9e27a28baac9
Douglas Bruno <dougbrunos@gmail.com>
2025-08-19 17:37:44 -0300

    Examples StreamAPI

---
+++ stream-api/src/main/java/com/douglas/interfaces_funcionais/SupplierExample.java
@@ -0,0 +1,17 @@
+package com.douglas.interfaces_funcionais;
+
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class SupplierExample {
+
+    public static void main(String[] args) {
+        Supplier<String> saudacao = () -> "Olá, seja bem-vindo!";
+
+        List<String> listaSaudacoes = Stream.generate(saudacao).limit(5).toList();
+
+        listaSaudacoes.forEach(System.out::println);
+    }
+
+}
